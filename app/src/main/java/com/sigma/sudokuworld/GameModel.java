eb4c14f360bdc2fbb9e625dd096808d4d40b7dcc
adam-watkins <cadamrun@gmail.com>
2019-01-27 01:53:54 -0800

    Practice Set Class

--- app/src/main/java/com/sigma/sudokuworld/GameModel.java
+++ app/src/main/java/com/sigma/sudokuworld/GameModel.java
@@ -1,27 +1,36 @@
 package com.sigma.sudokuworld;
 
-import android.util.Log;
-
 class GameModel {
 
     static final int SUDOKU_ROOT_SIZE = 2;
     static final int SUDOKU_SIZE = SUDOKU_ROOT_SIZE * SUDOKU_ROOT_SIZE;
+    private static final int SUDOKU_NUMBER_OF_CELLS = SUDOKU_SIZE * SUDOKU_SIZE;
 
     private int[] cellValues;
     private int numFilledCells = 0;
+    private boolean isGameWon;
+
+    static int cellNumToXPosition(int cellNumber) {
+        return cellNumber % SUDOKU_SIZE;
+    }
+
+    static int cellNumToYPosition(int cellNumber) {
+        return cellNumber / SUDOKU_SIZE;
+    }
 
     GameModel(){
-        cellValues = new int[SUDOKU_SIZE * SUDOKU_SIZE];
+        cellValues = new int[SUDOKU_NUMBER_OF_CELLS];
+        isGameWon = false;
     }
 
     void setValue(int x, int y, int number) {
-        Log.d("Set value", String.format("X: %d Y: %d N: %d", x, y, number));
          if (cellValues[(y * SUDOKU_SIZE) + x] == 0 && number != 0)
              numFilledCells++;
          else if (cellValues[(y * SUDOKU_SIZE) + x] != 0 && number == 0)
              numFilledCells--;
 
         cellValues[(y * SUDOKU_SIZE) + x] = number;
+        isGameWon = checkWinConditions();
     }
 
     int getValue(int x, int y) {
@@ -46,11 +55,16 @@ class GameModel {
         }
     }
 
-    static int cellNumToXPosition(int cellNumber) {
-        return cellNumber % SUDOKU_SIZE;
+    private boolean checkWinConditions() {
+        if (numFilledCells != SUDOKU_NUMBER_OF_CELLS)
+            return false;
+
+        //Check win conditions for row, col, and square here
+
+        return true;
     }
 
-    static int cellNumToYPosition(int cellNumber) {
-        return cellNumber / SUDOKU_SIZE;
+    public boolean isGameWon() {
+        return isGameWon;
     }
 }
