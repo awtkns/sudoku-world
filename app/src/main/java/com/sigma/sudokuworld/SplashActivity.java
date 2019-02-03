c0d8a64a3621e6642bde9efc0868936908a505b2
adam-watkins <cadamrun@gmail.com>
2019-02-03 13:27:59 -0800

    Splash screen with animation

--- app/src/main/java/com/sigma/sudokuworld/SplashActivity.java
+++ app/src/main/java/com/sigma/sudokuworld/SplashActivity.java
@@ -2,8 +2,10 @@ package com.sigma.sudokuworld;
 
 import android.content.Intent;
 import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
@@ -14,11 +16,17 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         ImageView imageView = findViewById(R.id.sigmaAnimation);
-        AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) imageView.getDrawable();
-        animatedVectorDrawable.start();
+        final AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) imageView.getDrawable();
 
-        Intent i = new Intent(this, MenuActivity.class);
-        //startActivity(i);
-        //finish();
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animatedVectorDrawable.start();
+            }
+        });
+
+
+//        Intent i = new Intent(this, MenuActivity.class);
+//        startActivity(i);
     }
 }
