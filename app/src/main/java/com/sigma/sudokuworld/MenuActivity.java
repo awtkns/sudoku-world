483379b1d8774e5c146c794dfd6781340e594e2e
adam-watkins <cadamrun@gmail.com>
2019-02-05 02:24:45 -0800

    Fixed spelling inconsistencies

--- app/src/main/java/com/sigma/sudokuworld/MenuActivity.java
+++ app/src/main/java/com/sigma/sudokuworld/MenuActivity.java
@@ -15,7 +15,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
 
