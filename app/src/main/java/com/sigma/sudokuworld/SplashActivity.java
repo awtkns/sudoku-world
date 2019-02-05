616dc1f6425a15c4e62f006407a29c4dcb22fda4
adam-watkins <cadamrun@gmail.com>
2019-02-05 00:28:56 -0800

    Transitions to menu after animation

--- app/src/main/java/com/sigma/sudokuworld/SplashActivity.java
+++ app/src/main/java/com/sigma/sudokuworld/SplashActivity.java
@@ -2,11 +2,12 @@ package com.sigma.sudokuworld;
 
 import android.content.Intent;
 import android.graphics.drawable.AnimatedVectorDrawable;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -18,15 +19,33 @@ public class SplashActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.sigmaAnimation);
         final AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) imageView.getDrawable();
 
-        imageView.setOnClickListener(new View.OnClickListener() {
+        //Timer starts animation on launch and exits splash screen
+        TimerTask animationStartTask = new TimerTask() {
             @Override
-            public void onClick(View v) {
+            public void run() {
                 animatedVectorDrawable.start();
             }
-        });
+        };
 
+        TimerTask exitSplashTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MenuActivity.class);
+                startActivity(i);
+            }
+        };
 
-//        Intent i = new Intent(this, MenuActivity.class);
-//        startActivity(i);
+        Timer timer = new Timer();
+        timer.schedule(animationStartTask, 1500);
+        timer.schedule(exitSplashTask, 5000);
+    }
+
+    /**
+     * Close the activity
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
