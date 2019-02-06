1bef31f61a3f71ae08c02c9aa5987396926bfbb7
Asim Shrestha <ashresth@sfu.ca>
2019-02-06 14:36:22 -0800

    Made loading settings work by pressing back

--- app/src/main/java/com/sigma/sudokuworld/SettingsActivity.java
+++ app/src/main/java/com/sigma/sudokuworld/SettingsActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
@@ -15,7 +13,7 @@ public class SettingsActivity extends AppCompatActivity {
     static final String DIFFICULTY_INTENT_KEY = "difficulty";
     static final String GAME_MODE_INTENT_KEY = "mode";
 
-    Button mBackButton;
+
     RadioGroup mGameModeRadioGroup;
     SeekBar mDifficultySeekBar;
 
@@ -26,42 +24,37 @@ public class SettingsActivity extends AppCompatActivity {
 
         mGameModeRadioGroup = findViewById(R.id.modeSelector);
         mDifficultySeekBar = findViewById(R.id.difficultyBar);
-        mBackButton = findViewById(R.id.loadWordList);
-        mBackButton.setOnClickListener(onBackButtonClickListener);
-
     }
 
-    View.OnClickListener onBackButtonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+    @Override
+    public void onBackPressed() {
 
-            //Checking GameMode
-            GameMode gameMode;
-            int checkedRadioButtonID = mGameModeRadioGroup.getCheckedRadioButtonId();
-            if (checkedRadioButtonID == R.id.nativeMode) {
-                gameMode = GameMode.NATIVE;
-            } else if (checkedRadioButtonID == R.id.foreignMode) {
-                gameMode = GameMode.FOREIGN;
-            } else {
-                gameMode = GameMode.NUMBERS;
-            }
-
-            //Checking Difficulty
-            GameDifficulty gameDifficulty;
-            int difficulty = mDifficultySeekBar.getProgress();
-            if (difficulty == 1) {
-                gameDifficulty = GameDifficulty.MEDIUM;
-            } else if (difficulty == 2) {
-                gameDifficulty = GameDifficulty.HARD;
-            } else {
-                gameDifficulty = GameDifficulty.EASY;
-            }
-
-            Intent result = getIntent();
-            result.putExtra(GAME_MODE_INTENT_KEY, gameMode);
-            result.putExtra(DIFFICULTY_INTENT_KEY, gameDifficulty);
-            setResult(RESULT_OK, result);
-            finish();
+        //Checking GameMode
+        GameMode gameMode;
+        int checkedRadioButtonID = mGameModeRadioGroup.getCheckedRadioButtonId();
+        if (checkedRadioButtonID == R.id.nativeMode) {
+            gameMode = GameMode.NATIVE;
+        } else if (checkedRadioButtonID == R.id.foreignMode) {
+            gameMode = GameMode.FOREIGN;
+        } else {
+            gameMode = GameMode.NUMBERS;
         }
-    };
+
+        //Checking Difficulty
+        GameDifficulty gameDifficulty;
+        int difficulty = mDifficultySeekBar.getProgress();
+        if (difficulty == 1) {
+            gameDifficulty = GameDifficulty.MEDIUM;
+        } else if (difficulty == 2) {
+            gameDifficulty = GameDifficulty.HARD;
+        } else {
+            gameDifficulty = GameDifficulty.EASY;
+        }
+
+        Intent settings = getIntent();
+        settings.putExtra(GAME_MODE_INTENT_KEY, gameMode);
+        settings.putExtra(DIFFICULTY_INTENT_KEY, gameDifficulty);
+        setResult(Activity.RESULT_OK, settings);
+        finish();
+    }
 }
