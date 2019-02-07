75881f275975ed0839ae0a0305437bda898873ce
adam-watkins <cadamrun@gmail.com>
2019-02-06 22:38:32 -0800

    Saves and loads game data

--- app/src/main/java/com/sigma/sudokuworld/KeyConstants.java
+++ app/src/main/java/com/sigma/sudokuworld/KeyConstants.java
@@ -14,7 +14,6 @@ final class KeyConstants {
      * A "save" Bundle should have:
      * Difficulty
      * GameMode
-     * Size
      * Cell values
      * Solution
      * Locked cells
@@ -24,13 +23,12 @@ final class KeyConstants {
      */
 
     //Public index keys for saving data in bundle
+    static final String DIFFICULTY_KEY = "difficulty";
+    static final String MODE_KEY = "mode";
     static final String SUDOKU_SIZE_KEY = "size";
     static final String CELL_VALUES_KEY = "values";
     static final String SOLUTION_VALUES_KEY = "solution";
     static final String LOCKED_CELLS_KEY = "locked";
     static final String NATIVE_WORDS_KEY = "native";
     static final String FOREIGN_WORDS_KEY = "foreign";
-    static final String MODE_KEY = "mode";
-    static final String DIFFICULTY_KEY = "difficulty";
-
 }
--- app/src/main/java/com/sigma/sudokuworld/PersistenceService.java
+++ app/src/main/java/com/sigma/sudokuworld/PersistenceService.java
@@ -16,14 +16,16 @@ public abstract class PersistenceService  {
     private static final String SAVE_GAME_FILE = "save";
     private static final String SAVE_SETTINGS_FILE = "settings";
 
+    //Private array size keys
+    private static final String WORD_LIST_SIZE = "word_size";
+    private static final String SUDOKU_SIZE = "sudoku_size";
+
     //Private index keys for read / writing arrays to xml
     private static final String VAlUES_KEY_PREFIX = "valueCell";
     private static final String SOLUTION_KEY_PREFIX = "solutionCell";
     private static final String LOCKED_KEY_PREFIX = "lockedCell";
     private static final String NATIVE_KEY_PREFIX = "nativeWord";
     private static final String FOREIGN_KEY_PREFIX = "foreignWord";
-
-
 
     static void saveGameData(Context context, Bundle data) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(SAVE_GAME_FILE, Context.MODE_PRIVATE);
@@ -32,16 +34,20 @@ public abstract class PersistenceService  {
         //Unpacking "save" bundle
         GameDifficulty difficulty = (GameDifficulty) data.getSerializable(DIFFICULTY_KEY);
         GameMode mode = (GameMode) data.getSerializable(MODE_KEY);
-        int arraySize = data.getInt(SUDOKU_SIZE_KEY);
         int[] cellValues = data.getIntArray(CELL_VALUES_KEY);
         int[] solutionValues = data.getIntArray(SOLUTION_VALUES_KEY);
         boolean[] lockedCells = data.getBooleanArray(LOCKED_CELLS_KEY);
         String[] nativeWords = data.getStringArray(NATIVE_WORDS_KEY);
         String[] foreignWords = data.getStringArray(FOREIGN_WORDS_KEY);
 
+        int arraySize = cellValues.length;
+        int wordListSize = nativeWords.length;
+
+        //Writing game constants
         editor.putString(DIFFICULTY_KEY, difficulty.name());
         editor.putString(MODE_KEY, mode.name());
         editor.putInt(SUDOKU_SIZE_KEY, arraySize);
+        editor.putInt(WORD_LIST_SIZE, wordListSize);
 
         //Writing cell data
         for (int i=0; i < arraySize; i++) {
@@ -52,7 +58,7 @@ public abstract class PersistenceService  {
         }
 
         //Writing word list
-        for (int i=0; i < nativeWords.length; i++) {
+        for (int i=0; i < wordListSize; i++) {
             String index = Integer.toString(i);
             editor.putString(NATIVE_KEY_PREFIX + index, nativeWords[i]);
             editor.putString(FOREIGN_KEY_PREFIX + index, foreignWords[i]);
@@ -67,8 +73,42 @@ public abstract class PersistenceService  {
         Bundle data = new Bundle();
 
         Map<String, ?> dataMap = sharedPreferences.getAll();
+
+        GameDifficulty difficulty = GameDifficulty.valueOf((String) dataMap.get(DIFFICULTY_KEY));
+        GameMode mode = GameMode.valueOf((String) dataMap.get(MODE_KEY));
+
+        //Loading game constants
         int size = (Integer) dataMap.get(SUDOKU_SIZE_KEY);
-        data.putInt(SUDOKU_SIZE_KEY, size);
+        int wordListSize = (Integer) dataMap.get(WORD_LIST_SIZE);
+
+        int[] cellValues = new int[size];
+        int[] solutionCells = new int[size];
+        boolean[] lockedCells = new boolean[size];
+        String[] nativeWords = new String[wordListSize];
+        String[] foreignWords = new String[wordListSize];
+
+        //Loading cell values
+        for (int i = 0; i < size; i++) {
+            String index = Integer.toString(i);
+            cellValues[i] = (Integer) dataMap.get(VAlUES_KEY_PREFIX + index);
+            solutionCells[i] = (Integer) dataMap.get(SOLUTION_KEY_PREFIX + index);
+            lockedCells[i] = (Boolean) dataMap.get(LOCKED_KEY_PREFIX + index);
+        }
+
+        //Loading word list
+        for (int i=0; i < wordListSize; i++) {
+            String index = Integer.toString(i);
+            nativeWords[i] = (String) dataMap.get(NATIVE_KEY_PREFIX + index);
+            foreignWords[i] = (String) dataMap.get(FOREIGN_KEY_PREFIX + index);
+        }
+
+        data.putSerializable(DIFFICULTY_KEY, difficulty);
+        data.putSerializable(MODE_KEY, mode);
+        data.putIntArray(CELL_VALUES_KEY, cellValues);
+        data.putIntArray(SOLUTION_VALUES_KEY, solutionCells);
+        data.putBooleanArray(LOCKED_CELLS_KEY, lockedCells);
+        data.putStringArray(NATIVE_WORDS_KEY, nativeWords);
+        data.putStringArray(FOREIGN_WORDS_KEY, foreignWords);
 
         return data;
     }
