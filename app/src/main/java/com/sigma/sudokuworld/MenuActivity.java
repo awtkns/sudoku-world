7536effe8de375538f19245e2201f8b75fe4008a
adam-watkins <cadamrun@gmail.com>
2019-02-07 18:22:29 -0800

    Settings Saving Finished

--- app/src/main/java/com/sigma/sudokuworld/MenuActivity.java
+++ app/src/main/java/com/sigma/sudokuworld/MenuActivity.java
@@ -11,23 +11,11 @@ import android.widget.ImageView;
 
 import com.sigma.sudokuworld.Persistence.KeyConstants;
 import com.sigma.sudokuworld.Persistence.PersistenceService;
-import com.sigma.sudokuworld.VocabGame.GameDifficulty;
-import com.sigma.sudokuworld.VocabGame.GameMode;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-
-
 
 public class MenuActivity extends AppCompatActivity {
 
-    private final int REQUEST_CODE = 1;
-    private GameDifficulty gameDifficulty = GameDifficulty.EASY;
-    private GameMode gameMode = GameMode.NUMBERS;
+    private static final String TAG = "MENU";
+    private static final int REQUEST_CODE = 1;
 
     Button mPlayButton;
     Button mContinueButton;
@@ -49,11 +37,9 @@ public class MenuActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MenuActivity.this, SudokuActivity.class);
 
-                //Adding information for the sudoku activity
-                intent.putExtra(KeyConstants.NATIVE_WORDS_KEY, readWordListFromCSV(WordType.NATIVE));
-                intent.putExtra(KeyConstants.FOREIGN_WORDS_KEY, readWordListFromCSV(WordType.FOREIGN));
-                intent.putExtra(KeyConstants.MODE_KEY, gameMode);
-                intent.putExtra(KeyConstants.DIFFICULTY_KEY, gameDifficulty);
+                //Settings bundle
+                Bundle settings = PersistenceService.loadSettingsData(MenuActivity.this);
+                intent.putExtras(settings);
                 intent.putExtra(KeyConstants.CONTINUE_KEY, false);
                 startActivity(intent);
             }
@@ -63,15 +49,14 @@ public class MenuActivity extends AppCompatActivity {
         mContinueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, SudokuActivity.class);
-
                 try {
+                    Intent intent = new Intent(MenuActivity.this, SudokuActivity.class);
                     intent.putExtras(PersistenceService.loadGameData(MenuActivity.this));
                     intent.putExtra(KeyConstants.CONTINUE_KEY, true);
-                    Log.d("Game Data", "onClick: starting game with data");
+                    Log.d(TAG, "onContinueClick: starting game with data");
                     startActivity(intent);
                 } catch (Exception e) {
-                    Log.d("Game Data", "onClick: no game data");
+                    Log.d(TAG, "onContinueClick: no game data");
                 }
             }
         });
@@ -84,94 +69,6 @@ public class MenuActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE);
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                gameDifficulty = (GameDifficulty) data.getSerializableExtra(KeyConstants.DIFFICULTY_KEY);
-                gameMode = (GameMode) data.getSerializableExtra(KeyConstants.MODE_KEY);
-            }
-        }
-    }
-
-    /**
-     * Reads puzzle data from csv file "simplepuzzles.csv"
-     * Stores data in an array.
-     * Array index = cell number
-     * Stores 0 for empty cell
-     *
-     * @return array
-     */
-    private int[] readPuzzleDataFromCSV() {
-        InputStream inputStream = getResources().openRawResource(R.raw.simplepuzzles);
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-
-        int[] vals = new int[0];
-        try {
-            String puzzleStr;
-            puzzleStr = bufferedReader.readLine();
-            puzzleStr = puzzleStr.replace(",", "");  //input sanitation
-
-            vals = new int[puzzleStr.length()];
-            for (int i = 0; i < puzzleStr.length(); i++) {   //read puzzle data
-                char ch = puzzleStr.charAt(i);
-                if (ch != '.') {
-                    vals[i] = Character.getNumericValue(ch);
-                }
-            }
-
-        } catch (IOException e) {
-            Log.e("MenuActivity", "readPuzzleDataFromCSV: cannot read file");
-        }
-
-        return vals;
-    }
-
-    private enum WordType {
-        NATIVE, FOREIGN
-    }
-
-    /**
-     * Reads CSV file containing 9 word pairs
-     *
-     * @param wordType get the native or foreign list
-     * @return string array of words in either the native or foreign lang
-     */
-    private String[] readWordListFromCSV(WordType wordType) {
-        InputStream inputStream = getResources().openRawResource(R.raw.wordlist);
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-        String line;
-        ArrayList<String> wordList = new ArrayList<>();
-        try {
-            while ((line = bufferedReader.readLine()) != null) {
-                if (wordType == WordType.NATIVE) {
-                    int i = line.indexOf(',');
-
-                    if (i != -1) {
-                        wordList.add(line.substring(0, i));
-                    }
-                } else if (wordType == WordType.FOREIGN) {
-                    int i = line.indexOf(',');
-
-                    if (i != -1) {
-                        wordList.add(line.substring(i + 1));
-                    }
-                }
-            }
-        } catch (IOException e) {
-            Log.e("MenuActivity", "readWordListFromCSV: cannot read file");
-        }
-
-        String[] words = new String[wordList.size()];
-        return wordList.toArray(words);
     }
 }
 
--- app/src/main/java/com/sigma/sudokuworld/SettingsActivity.java
+++ app/src/main/java/com/sigma/sudokuworld/SettingsActivity.java
@@ -8,6 +8,7 @@ import android.widget.RadioGroup;
 import android.widget.SeekBar;
 
 import com.sigma.sudokuworld.Persistence.KeyConstants;
+import com.sigma.sudokuworld.Persistence.PersistenceService;
 import com.sigma.sudokuworld.VocabGame.GameDifficulty;
 import com.sigma.sudokuworld.VocabGame.GameMode;
 
@@ -21,8 +22,28 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        mGameModeRadioGroup = findViewById(R.id.modeSelector);
+        mGameModeRadioGroup = findViewById(R.id.gameModeRadioGroup);
         mDifficultySeekBar = findViewById(R.id.difficultyBar);
+
+        Bundle previousSettings = PersistenceService.loadSettingsData(SettingsActivity.this);
+        GameDifficulty gameDifficulty = (GameDifficulty) previousSettings.getSerializable(KeyConstants.DIFFICULTY_KEY);
+        GameMode gameMode = (GameMode) previousSettings.getSerializable(KeyConstants.MODE_KEY);
+
+        if (gameDifficulty == GameDifficulty.EASY) {
+            mDifficultySeekBar.setProgress(0);
+        } else if (gameDifficulty == GameDifficulty.MEDIUM) {
+            mDifficultySeekBar.setProgress(1);
+        } else  if (gameDifficulty == GameDifficulty.HARD) {
+            mDifficultySeekBar.setProgress(2);
+        }
+
+        if (gameMode == GameMode.NATIVE) {
+            mGameModeRadioGroup.check(R.id.nativeModeRadioButton);
+        } else if (gameMode == GameMode.FOREIGN) {
+            mGameModeRadioGroup.check(R.id.foreignModeRadioButton);
+        } else {
+            mGameModeRadioGroup.check(R.id.numbersModeRadioButton);
+        }
     }
 
     @Override
@@ -31,9 +52,9 @@ public class SettingsActivity extends AppCompatActivity {
         //Checking GameMode
         GameMode gameMode;
         int checkedRadioButtonID = mGameModeRadioGroup.getCheckedRadioButtonId();
-        if (checkedRadioButtonID == R.id.nativeMode) {
+        if (checkedRadioButtonID == R.id.nativeModeRadioButton) {
             gameMode = GameMode.NATIVE;
-        } else if (checkedRadioButtonID == R.id.foreignMode) {
+        } else if (checkedRadioButtonID == R.id.foreignModeRadioButton) {
             gameMode = GameMode.FOREIGN;
         } else {
             gameMode = GameMode.NUMBERS;
@@ -50,10 +71,14 @@ public class SettingsActivity extends AppCompatActivity {
             gameDifficulty = GameDifficulty.EASY;
         }
 
-        Intent settings = getIntent();
-        settings.putExtra(KeyConstants.MODE_KEY, gameMode);
-        settings.putExtra(KeyConstants.DIFFICULTY_KEY, gameDifficulty);
-        setResult(Activity.RESULT_OK, settings);
+        //Saving Data
+        Bundle settingsBundle = new Bundle();
+        settingsBundle.putSerializable(KeyConstants.DIFFICULTY_KEY, gameDifficulty);
+        settingsBundle.putSerializable(KeyConstants.MODE_KEY, gameMode);
+        PersistenceService.saveSettingsData(SettingsActivity.this, settingsBundle);
+
+        Intent intent = getIntent();
+        setResult(Activity.RESULT_OK, intent);
         finish();
     }
 }
