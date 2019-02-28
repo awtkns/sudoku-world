bce5fa001f198db3ca1b61ab0fc05bb35643e4e9
adam-watkins <cadamrun@gmail.com>
2019-02-28 14:08:14 -0800

    Room Dependencies Added

---
+++ app/src/main/java/com/sigma/sudokuworld/App.java
@@ -0,0 +1,11 @@
+package com.sigma.sudokuworld;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
