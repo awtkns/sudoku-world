011f9fe6c74a2f26eb59bb9a5d04ca7185e13d07
adam-watkins <cadamrun@gmail.com>
2019-03-04 23:52:55 -0800

    Added extensive game save tests. Refactored test classes

--- app/src/androidTest/java/com/sigma/sudokuworld/DatabaseInstrumentedTest.kt
+++ app/src/androidTest/java/com/sigma/sudokuworld/DatabaseInstrumentedTest.kt
@@ -21,6 +21,11 @@ import java.lang.Exception
 @RunWith(AndroidJUnit4::class)
 class DatabaseInstrumentedTest {
     private lateinit var db: AppDatabase
+    private lateinit var languageList: List<Language>
+    private lateinit var wordList: List<Word>
+    private lateinit var wordPairList: List<WordPair>
+    private lateinit var setList: List<Set>
+    private lateinit var wordSetList: List<WordSet>
 
     @Before
     fun createDb() {
@@ -39,10 +44,12 @@ class DatabaseInstrumentedTest {
     @Throws(Exception::class)
     fun readLanguage() {
         val storedLanguages = db.getLanguageDao().getAll()
-        assertEquals("English", storedLanguages[0].name)
-        assertEquals("en", storedLanguages[0].code)
-        assertEquals("French", storedLanguages[1].name)
-        assertEquals("fr", storedLanguages[1].code)
+        assertEquals(languageList[0].name, storedLanguages[0].name)
+        assertEquals(languageList[0].code, storedLanguages[0].code)
+        assertEquals(languageList[1].name, storedLanguages[1].name)
+        assertEquals(languageList[1].code, storedLanguages[1].code)
+        assertNotEquals(languageList[1].name, storedLanguages[0].name)
+        assertNotEquals(languageList[1].code, storedLanguages[0].code)
     }
 
     @Test
@@ -60,30 +67,37 @@ class DatabaseInstrumentedTest {
     @Throws(Exception::class)
     fun readWord() {
         val storedWords = db.getWordDao().getAll()
-        assertEquals("en_word", storedWords[0].word)
-        assertEquals(1, storedWords[0].languageID)
-        assertEquals(1, storedWords[0].wordID)
-        assertEquals("fr_word", storedWords[1].word)
-        assertEquals(2, storedWords[1].languageID)
-        assertEquals(2, storedWords[1].wordID)
+        assertEquals(wordList[0].word, storedWords[0].word)
+        assertEquals(wordList[0].languageID, storedWords[0].languageID)
+        assertEquals(wordList[0].wordID, storedWords[0].wordID)
+        assertEquals(wordList[1].word, storedWords[1].word)
+        assertEquals(wordList[1].languageID, storedWords[1].languageID)
+        assertEquals(wordList[1].wordID, storedWords[1].wordID)
+        assertNotEquals(wordList[1].word, storedWords[0].word)
+        assertNotEquals(wordList[1].languageID, storedWords[0].languageID)
+        assertNotEquals(wordList[1].wordID, storedWords[0].wordID)
     }
 
     @Test
     @Throws(Exception::class)
     fun readWordPair() {
         val storedWordPairs = db.getWordPairDao().getAll()
-        assertEquals(1, storedWordPairs[0].wordPairID)
-        assertEquals(1, storedWordPairs[0].nativeWordID)
-        assertEquals(2, storedWordPairs[0].foreignWordID)
+        assertEquals(wordPairList[0].wordPairID, storedWordPairs[0].wordPairID)
+        assertEquals(wordPairList[0].nativeWordID, storedWordPairs[0].nativeWordID)
+        assertEquals(wordPairList[0].foreignWordID, storedWordPairs[0].foreignWordID)
+        assertEquals(wordPairList[1].wordPairID, storedWordPairs[1].wordPairID)
+        assertEquals(wordPairList[1].nativeWordID, storedWordPairs[1].nativeWordID)
+        assertEquals(wordPairList[1].foreignWordID, storedWordPairs[1].foreignWordID)
+        assertNotEquals(wordPairList[1].wordPairID, storedWordPairs[0].wordPairID)
     }
 
     @Test
     @Throws(Exception::class)
     fun readSet() {
         val storedSets = db.getSetDao().getAll()
-        assertEquals( 1, storedSets[0].setID)
-        assertEquals("test_set_name", storedSets[0].name)
-        assertEquals("test_set_description", storedSets[0].description)
+        assertEquals( setList[0].setID, storedSets[0].setID)
+        assertEquals(setList[0].name, storedSets[0].name)
+        assertEquals(setList[0].description, storedSets[0].description)
     }
 
     /**
@@ -100,34 +114,85 @@ class DatabaseInstrumentedTest {
     @Test
     @Throws(Exception::class)
     fun readGameSave() {
-        val save = Game(1, 1, GameDifficulty.MEDIUM, GameMode.FOREIGN, IntArray(0), IntArray(0), BooleanArray(0))
-        db.getGameSaveDao().insert(save)
+
+        val save1 = Game(
+                1,
+                1,
+                GameDifficulty.EASY,
+                GameMode.NUMBERS,
+                IntArray(0),
+                IntArray(0),
+                BooleanArray(0))
+        val save2 = Game(
+                2,
+                1,
+                GameDifficulty.MEDIUM,
+                GameMode.NATIVE,
+                intArrayOf(1, 2, 3),
+                intArrayOf(1, 3, 3),
+                booleanArrayOf(true, false, true))
+
+        val gameList = listOf(save1, save2)
+
+
+        db.getGameSaveDao().insert(save1, save2)
 
         val gameSaves = db.getGameSaveDao().getAll()
-        assertEquals(1, gameSaves[0].saveID)
-        assertEquals(GameDifficulty.MEDIUM, gameSaves[0].difficulty)
-        assertEquals(GameMode.FOREIGN, gameSaves[0].gameMode)
+        assertEquals(gameList[0].saveID, gameSaves[0].saveID)
+        assertEquals(gameList[0].difficulty, gameSaves[0].difficulty)
+        assertEquals(gameList[0].gameMode, gameSaves[0].gameMode)
+        assertTrue(gameList[0].cellValues.contentEquals(gameSaves[0].cellValues))
+        assertTrue(gameList[0].solutionValues.contentEquals(gameSaves[0].solutionValues))
+        assertTrue(gameList[0].lockedCells.contentEquals(gameSaves[0].lockedCells))
+
+        assertEquals(gameList[1].saveID, gameSaves[1].saveID)
+        assertEquals(gameList[1].difficulty, gameSaves[1].difficulty)
+        assertEquals(gameList[1].gameMode, gameSaves[1].gameMode)
+        assertTrue(gameList[1].cellValues.contentEquals(gameSaves[1].cellValues))
+        assertTrue(gameList[1].solutionValues.contentEquals(gameSaves[1].solutionValues))
+        assertTrue(gameList[1].lockedCells.contentEquals(gameSaves[1].lockedCells))
+
+        assertNotEquals(gameList[0].saveID, gameSaves[1].saveID)
+
+        //Testing overridden equals() and hashcode()
+        assertEquals(gameList[0], gameSaves[0])
+        assertEquals(gameList[1], gameSaves[1])
+        assertNotEquals(gameList[0], gameSaves[1])
+        assertEquals(gameList[0].hashCode(), gameSaves[0].hashCode())
+        assertEquals(gameList[1].hashCode(), gameSaves[1].hashCode())
+        assertNotEquals(gameList[0].hashCode(), gameSaves[1].hashCode())
+
+        //Testing array getters
+        assertEquals(gameList[1].cellValues[1], gameList[1].getCellValue(1))
+        assertEquals(gameList[1].lockedCells[0], gameList[1].isLocked(0))
+        assertEquals(gameList[1].lockedCells[1], gameList[1].isLocked(1))
     }
 
 
     private fun populateDatabase() {
+
         val lang1 = Language(1, "English", "en")
         val lang2 = Language(2, "French", "fr")
+        languageList = listOf(lang1, lang2)
         db.getLanguageDao().insert(lang1, lang2)
 
         val word1 = Word(1, 1, "en_word")
         val word2 = Word(2, 2, "fr_word")
+        wordList = listOf(word1, word2)
         db.getWordDao().insert(word1, word2)
 
         val wordPair1 = WordPair(1, 1, 2)
         val wordPair2 = WordPair(2, 2, 1)
+        wordPairList = listOf(wordPair1, wordPair2)
         db.getWordPairDao().insert(wordPair1, wordPair2)
 
-        val set = Set(1, "test_set_name", "test_set_description")
-        db.getSetDao().insert(set)
+        val set1 = Set(1, "test_set_name", "test_set_description")
+        setList = listOf(set1)
+        db.getSetDao().insert(set1)
 
         val wordSet1 = WordSet(1, 1)
         val wordSet2 = WordSet(1, 2)
+        wordSetList = listOf(wordSet1, wordSet2)
         db.getWordSetDao().insert(wordSet1, wordSet2)
     }
 }
