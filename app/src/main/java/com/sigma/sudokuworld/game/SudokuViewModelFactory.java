882e3b088da9bac24593fd6333304791a6befde4
adam-watkins <cadamrun@gmail.com>
2019-03-05 11:50:03 -0800

    Factory to create SudokuViewModels with custom parameters

---
+++ app/src/main/java/com/sigma/sudokuworld/game/SudokuViewModelFactory.java
@@ -0,0 +1,24 @@
+package com.sigma.sudokuworld.game;
+
+import android.app.Application;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
+
+public class SudokuViewModelFactory implements ViewModelProvider.Factory {
+
+    private final Application application;
+    private final int saveId;
+
+    public SudokuViewModelFactory(Application application, int saveId) {
+        this.saveId = saveId;
+        this.application = application;
+    }
+
+    @SuppressWarnings("unchecked")
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new SudokuViewModel(application, saveId);
+    }
+}
