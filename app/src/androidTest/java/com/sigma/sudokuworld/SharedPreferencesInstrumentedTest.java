64a8c6b8a3810ce9108d082831d3ff51e0eaaad4
adam-watkins <cadamrun@gmail.com>
2019-03-07 00:51:33 -0800

    Upgraded to persistent service 2.0 Tests

--- app/src/androidTest/java/com/sigma/sudokuworld/SharedPreferencesInstrumentedTest.java
+++ app/src/androidTest/java/com/sigma/sudokuworld/SharedPreferencesInstrumentedTest.java
@@ -1,7 +1,6 @@
 package com.sigma.sudokuworld;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -13,14 +12,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static com.sigma.sudokuworld.persistence.sharedpreferences.KeyConstants.AUDIO_KEY;
-import static com.sigma.sudokuworld.persistence.sharedpreferences.KeyConstants.DIFFICULTY_KEY;
-import static com.sigma.sudokuworld.persistence.sharedpreferences.KeyConstants.HINTS_KEY;
-import static com.sigma.sudokuworld.persistence.sharedpreferences.KeyConstants.MODE_KEY;
-import static com.sigma.sudokuworld.persistence.sharedpreferences.KeyConstants.SOUND_KEY;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+@SuppressWarnings("ConstantConditions")
 @RunWith(AndroidJUnit4.class)
 public class SharedPreferencesInstrumentedTest {
     private Context context;
@@ -30,37 +25,77 @@ public class SharedPreferencesInstrumentedTest {
         context = InstrumentationRegistry.getContext();
     }
 
-    @SuppressWarnings("ConstantConditions")
-    @Test
-    public void readWriteSettings() {
-        Bundle data = new Bundle();
 
-        GameMode gameMode = GameMode.FOREIGN;
+    @Test
+    public void readWriteDifficulty() {
         GameDifficulty difficulty = GameDifficulty.HARD;
+        PersistenceService.saveDifficultySetting(context, difficulty);
+        assertEquals(difficulty, PersistenceService.loadDifficultySetting(context));
+
+        difficulty = GameDifficulty.MEDIUM;
+        PersistenceService.saveDifficultySetting(context, difficulty);
+        assertEquals(difficulty, PersistenceService.loadDifficultySetting(context));
+
+        difficulty = GameDifficulty.EASY;
+        PersistenceService.saveDifficultySetting(context, difficulty);
+        assertEquals(difficulty, PersistenceService.loadDifficultySetting(context));
+    }
+
+    @Test
+    public void readWriteGameMode() {
+        GameMode gameMode = GameMode.FOREIGN;
+        PersistenceService.saveGameModeSetting(context, gameMode);
+        assertEquals(gameMode, PersistenceService.loadGameModeSetting(context));
+        assertNotEquals(GameMode.opposite(gameMode), PersistenceService.loadGameModeSetting(context));
+
+        gameMode = GameMode.NATIVE;
+        PersistenceService.saveGameModeSetting(context, gameMode);
+        assertEquals(gameMode, PersistenceService.loadGameModeSetting(context));
+        assertNotEquals(GameMode.opposite(gameMode), PersistenceService.loadGameModeSetting(context));
+
+        gameMode = GameMode.NUMBERS;
+        PersistenceService.saveGameModeSetting(context, gameMode);
+        assertEquals(gameMode, PersistenceService.loadGameModeSetting(context));
+    }
+
+    @Test
+    public void readWriteAudioMode() {
         boolean isAudioMode = false;
+        PersistenceService.saveAudioModeEnableSetting(context, isAudioMode);
+        assertEquals(isAudioMode, PersistenceService.loadAudioModeSetting(context));
+        assertNotEquals(!isAudioMode, PersistenceService.loadAudioModeSetting(context));
+
+        isAudioMode = true;
+        PersistenceService.saveAudioModeEnableSetting(context, isAudioMode);
+        assertEquals(isAudioMode, PersistenceService.loadAudioModeSetting(context));
+        assertNotEquals(!isAudioMode, PersistenceService.loadAudioModeSetting(context));
+    }
+
+    @Test
+    public void readWriteSoundEnabled() {
         boolean isSoundOn = true;
+
+        PersistenceService.saveSoundEnabledSetting(context, isSoundOn);
+        assertEquals(isSoundOn, PersistenceService.loadSoundEnabledSetting(context));
+        assertNotEquals(!isSoundOn, PersistenceService.loadSoundEnabledSetting(context));
+
+        isSoundOn = true;
+        PersistenceService.saveSoundEnabledSetting(context, isSoundOn);
+        assertEquals(isSoundOn, PersistenceService.loadSoundEnabledSetting(context));
+        assertNotEquals(!isSoundOn, PersistenceService.loadSoundEnabledSetting(context));
+    }
+
+    @Test
+    public void readWriteHintsEnabled() {
         boolean isHintsOn = false;
 
+        PersistenceService.saveHintsEnabledSetting(context, isHintsOn);
+        assertEquals(isHintsOn, PersistenceService.loadHintsEnabledSetting(context));
+        assertNotEquals(!isHintsOn, PersistenceService.loadHintsEnabledSetting(context));
 
-        //Making settings bundle
-        data.putSerializable(DIFFICULTY_KEY, difficulty);
-        data.putSerializable(MODE_KEY, gameMode);
-        data.putBoolean(AUDIO_KEY, isAudioMode);
-        data.putBoolean(SOUND_KEY, isSoundOn);
-        data.putBoolean(HINTS_KEY, isHintsOn);
-
-        //Making sure that serializable data works
-        assertEquals(difficulty, data.getSerializable(DIFFICULTY_KEY));
-        assertEquals(gameMode, data.getSerializable(MODE_KEY));
-
-        PersistenceService.saveSettingsData(context, data);
-        Bundle settings = PersistenceService.loadSettingsData(context);
-
-        assertEquals(difficulty, settings.getSerializable(DIFFICULTY_KEY));
-        assertEquals(gameMode, settings.getSerializable(MODE_KEY));
-        assertEquals(isAudioMode, settings.getSerializable(AUDIO_KEY));
-        assertEquals(isSoundOn, settings.getSerializable(SOUND_KEY));
-        assertEquals(isHintsOn, settings.getSerializable(HINTS_KEY));
-        assertNotEquals(!isHintsOn, settings.getSerializable(HINTS_KEY));
+        isHintsOn = true;
+        PersistenceService.saveHintsEnabledSetting(context, isHintsOn);
+        assertEquals(isHintsOn, PersistenceService.loadHintsEnabledSetting(context));
+        assertNotEquals(!isHintsOn, PersistenceService.loadHintsEnabledSetting(context));
     }
 }
