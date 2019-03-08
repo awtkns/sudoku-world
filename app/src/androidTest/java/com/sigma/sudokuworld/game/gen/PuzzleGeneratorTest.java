95aadab579de65002be49b5c6e5d605d057e0cb6
kearrooooooooooooooo <32694622+kearrooooooooooooooo@users.noreply.github.com>
2019-03-07 22:58:16 -0800

    fixed puzzle generator test

--- app/src/androidTest/java/com/sigma/sudokuworld/game/gen/PuzzleGeneratorTest.java
+++ app/src/androidTest/java/com/sigma/sudokuworld/game/gen/PuzzleGeneratorTest.java
@@ -6,22 +6,20 @@ import com.sigma.sudokuworld.game.GameDifficulty;
 import com.sigma.sudokuworld.persistence.sharedpreferences.KeyConstants;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PuzzleGeneratorTest {
     private PuzzleGenerator puzzle = new PuzzleGenerator(3);
-
     @Test
     public void generatePuzzle() {
         Bundle b = puzzle.generatePuzzle(GameDifficulty.EASY);
-        boolean[] locked = new boolean[81];
-        assertEquals(81, b.size());
-//        Bundle bundle = puzzle.generatePuzzle(GameDifficulty.EASY);
-//        int value[] = bundle.getIntArray(KeyConstants.CELL_VALUES_KEY);
-//        assertEquals(value, puzzle.generatePuzzle(GameDifficulty.EASY));
-    }
+        int cells[]= b.getIntArray(KeyConstants.CELL_VALUES_KEY);
+        int solutions[]=b.getIntArray(KeyConstants.SOLUTION_VALUES_KEY);
+        boolean locked[]=b.getBooleanArray(KeyConstants.LOCKED_CELLS_KEY);
 
-    @Test
-    public void isCellVisited() {
+        assertEquals(81, cells.length);
+        assertEquals(81, solutions.length);
+        assertEquals(81, locked.length);
     }
 }
